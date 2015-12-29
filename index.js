bae21b4147a169e832c0ddd1dbc1dcc74e2bc392
Igor Borysyuk <borysyuk@qarea.com>
2015-12-29 16:34:40 +0200

    supporting external definition file has been added

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ var url = require('url')
   , Promise = require('bluebird')
   , _ = require('underscore')
   , registry = require('datapackage-registry')
+  , url = require("url")
   ;
+
+function getDefinitionUrl(schemaUrl) {
+  var urlComponents = url.parse(schemaUrl);
+  var tempArray = urlComponents.pathname.split('/');
+  tempArray.pop();
+  tempArray.push('definitions.json');
+  urlComponents.search = '';
+  urlComponents.pathname  = tempArray.join('/');
+  return url.format(urlComponents);
+}
 
 exports.validate = function(raw, schema) {
   var json = raw;
@@ -50,17 +61,27 @@ exports.validate = function(raw, schema) {
         return null;
       }
 
-      request(profile.schema, function(E, R, B) {
-        if(E) {
-          RJ('Failed loading schema from ' + profile.schema);
+      var definitionUrl = getDefinitionUrl(profile.schema);
+      request(definitionUrl, function(error, response, body){
+        if(error) {
+          RJ('Failed loading definition file from ' + definitionUrl);
           return null;
         }
+        request(profile.schema, function(E, R, B) {
+          if(E) {
+            RJ('Failed loading schema from ' + profile.schema);
+            return null;
+          }
 
-        try {
-          RS(tv4.validateMultiple(json, JSON.parse(B)));
-        } catch(E) {
-          RJ('Failed parsing schema json from ' + profile.schema);
-        }
+          try {
+            tv4.addSchema("definitions.json", JSON.parse(body));
+
+            RS(tv4.validateMultiple(json, JSON.parse(B)));
+          } catch(E) {
+            RJ('Failed parsing schema json from ' + profile.schema);
+          }
+        });
+
       });
     }, function() { RJ('Registry request failed') });
   })).then(function(R) {
